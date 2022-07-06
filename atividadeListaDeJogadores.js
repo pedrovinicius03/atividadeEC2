a7f514b0dfcf2c79bc668fe5d5c2226591ccd7e2
Pedro Vinicius <pedrovinicius883@gmail.com>
2022-07-06 15:11:39 -0300

    atividadeEC2

---
+++ atividadeListaDeJogadores.js
@@ -0,0 +1,7 @@
+let listaDeJogadores = ["Lebron James","James Harden","Stephen Curry","Kevin Durant","Giannis Antetokounmpo","Luka Dončić","Ja Morant","Nikola Jokic","Joel Embiid","Kyrie Irving"];
+
+for (let contador of listaDeJogadores) {
+    console.log("O nome do jogador é " + contador + " Da NBA");
+    
+}
+
